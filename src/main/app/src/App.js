b3353a2364737fd3695760de1596503825e592c4
petardev101 <workdev1989@gmail.com>
2017-10-02 16:07:01 -0500

    adding Tachyons

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Main from './components/Main'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <h2>React Spring Boot APP</h2>
+        <Main />
       </div>
     );
   }
---
+++ src/main/app/src/components/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Main extends Component {
+    render() {
+      return (
+        <div className="App">
+          <h4>Mathematical science shows what is. It is the language of unseen relations 
+            between things. But to use and apply that language, we must be able fully to 
+            appreciate, to feel, to seize the unseen, the unconscious.</h4>
+        </div>
+      )
+    }
+  }
+  
+  export default Main
