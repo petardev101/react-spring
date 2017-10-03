9b4a19dad33654aed13c44c930cc6ffbacdba2aa
petardev101 <workdev1989@gmail.com>
2017-10-03 12:17:34 -0500

    working on tachyons-css additions

--- src/main/app/src/components/Header.js
+++ src/main/app/src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 import logo from '../logo.jpg'
 
 export default () => (
-  <header className='tc pv4 pv5-ns'>
+  <header className='tc pv4 pv5-ns bg-light-blue .db p'>
     <img src={logo} className='br-100 pa1 ba b--black-10 h3 w3' alt='avatar' />
-    <h1 className='f5 f4-ns fw6 mid-gray'>Jasper Whitehouse</h1>
-    <h2 className='f6 gray fw2 ttu tracked'>Los Angeles</h2>
+    <h1 className='f5 f4-ns fw6'>Matt Larson</h1>
+    <h2 className='f6 gray fw2 i ttu tracked'>Twin Cities based Full Stack Software Engineer</h2>
   </header>
 )
--- src/main/app/src/components/Main.js
+++ src/main/app/src/components/Main.js
@@ -4,9 +4,11 @@ class Main extends Component {
     render() {
       return (
         <div className="App">
-          <h4>Mathematical science shows what is. It is the language of unseen relations 
+          <h4 className='f5 f4-ns fw6 mid-gray medium-narrow'>"Mathematical science shows what is. It is the language of unseen relations 
             between things. But to use and apply that language, we must be able fully to 
-            appreciate, to feel, to seize the unseen, the unconscious.</h4>
+            appreciate, to feel, to seize the unseen, the unconscious." -Ada Lovelace</h4>
+            <br />
+            <p className='fr mid-gray'>This site was built using ReactJS, Spring-Boot, Maven and Tachyons-CSS.</p>
         </div>
       )
     }
